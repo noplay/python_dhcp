7a77e31f791e140426ee98adf127c7ebc0c39393
apgmer <apgmer@gmail.com>
2016-04-24 08:39:39 +0800

    done

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
+# *-* coding:utf-8 *-*
 import socket
 import struct
 from uuid import getnode as get_mac
 from random import randint
+
 
 def getMacInBytes():
     mac = str(hex(get_mac()))
@@ -50,13 +52,87 @@ class DHCPDiscover:
         return packet
 
 
+class DHCPOffer:
+    def __init__(self, data, transID):
+        self.data = data
+        self.transID = transID
+        self.offerIP = ''
+        self.nextServerIP = ''
+        self.DHCPServerIdentifier = ''
+        self.leaseTime = ''
+        self.router = ''
+        self.subnetMask = ''
+        self.DNS = []
+        self.unpack()
+
+    def unpack(self):
+        if self.data[4:8] == self.transID:
+            self.offerIP = '.'.join(map(lambda x: str(x), data[16:20]))
+            self.nextServerIP = '.'.join(map(lambda x: str(x), data[20:24]))  # c'est une option
+            self.DHCPServerIdentifier = '.'.join(map(lambda x: str(x), data[245:249]))
+            self.leaseTime = str(struct.unpack('!L', data[251:255])[0])
+            self.router = '.'.join(map(lambda x: str(x), data[257:261]))
+            self.subnetMask = '.'.join(map(lambda x: str(x), data[263:267]))
+            dnsNB = int(data[268] / 4)
+            for i in range(0, 4 * dnsNB, 4):
+                self.DNS.append('.'.join(map(lambda x: str(x), data[269 + i:269 + i + 4])))
+
+    def printOffer(self):
+        key = ['DHCP Server', 'Offered IP address', 'subnet mask', 'lease time (s)', 'default gateway']
+        val = [self.DHCPServerIdentifier, self.offerIP, self.subnetMask, self.leaseTime, self.router]
+        for i in range(4):
+            print('{0:20s} : {1:15s}'.format(key[i], val[i]))
+
+        print('{0:20s}'.format('DNS Servers') + ' : ', end='')
+        if self.DNS:
+            print('{0:15s}'.format(self.DNS[0]))
+        if len(self.DNS) > 1:
+            for i in range(1, len(self.DNS)):
+                print('{0:22s} {1:15s}'.format(' ', self.DNS[i]))
+
+
+class DHCPRequest:
+    def __init__(self, transID, reqIp):
+        self.transID = transID
+        self.reqAddr = reqIp
+
+    def requestPackage(self):
+        macb = getMacInBytes()
+        packet = b''
+        packet += b'\x01'  # Message type: Boot Request (1)
+        packet += b'\x01'  # Hardware type: Ethernet
+        packet += b'\x06'  # Hardware address length: 6
+        packet += b'\x00'  # Hops: 0
+        packet += self.transID  # Transaction ID
+        packet += b'\x00\x00'  # Seconds elapsed: 0
+        packet += b'\x80\x00'  # Bootp flags: 0x8000 (Broadcast) + reserved flags
+        packet += b'\x00\x00\x00\x00'  # Client IP address: 0.0.0.0
+        packet += b'\x00\x00\x00\x00'  # Your (client) IP address: 0.0.0.0
+        packet += b'\x00\x00\x00\x00'  # Next server IP address: 0.0.0.0
+        packet += b'\x00\x00\x00\x00'  # Relay agent IP address: 0.0.0.0
+        # packet += b'\x00\x26\x9e\x04\x1e\x9b'   #Client MAC address: 00:26:9e:04:1e:9b
+        packet += macb
+        packet += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'  # Client hardware address padding: 00000000000000000000
+        packet += b'\x00' * 67  # Server host name not given
+        packet += b'\x00' * 125  # Boot file name not given
+        packet += b'\x63\x82\x53\x63'  # Magic cookie: DHCP
+        packet += b'\x35\x01\x03'  # Option: (t=53,l=1) DHCP Message Type = DHCP Discover
+        # packet += b'\x3d\x06\x00\x26\x9e\x04\x1e\x9b'   #Option: (t=61,l=6) Client identifier
+        packet += b'\x3d\x07\x01' + macb
+        packet += b'\x32\x04' + self.reqAddr # request ip addr
+        packet += b'\x0c\x02\x74\x74'
+        # packet += b'\x37\x03\x03\x01\x06'  # Option: (t=55,l=3) Parameter Request List
+        packet += b'\xff'  # End Option
+        return packet
+
+
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # 广播
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     try:
-        s.bind(('', 6800))
+        s.bind(('', 68))
     except Exception as e:
         print("port 68 is in use")
         s.close()
@@ -67,13 +143,24 @@ if __name__ == '__main__':
 
     # receiving DHCPOffer packet
     s.settimeout(2)
-    while 1:
-        data = s.recvfrom(1024)
-        if not data:
-            break
-        print(data)
+    try:
+        while True:
+            data = s.recv(1024)
+            m = map(lambda x: str(x), data[16:20])
+            print(str(data[16:18]))
+            offer = DHCPOffer(data, discoverPacket.transactionID)
+            if offer.offerIP:
+                print(offer.offerIP)
+                dhcprequest = DHCPRequest(discoverPacket.transactionID, data[16:20])
+                s.sendto(dhcprequest.requestPackage(), ('<broadcast>', 67))
+
+                while True:
+                    dd = s.recv(1024)
+                    # print(dd)
+                break
+    except socket.timeout as e:
+        print(e)
 
     s.close()  # we close the socket
-
     input('press any key to quit...')
     exit()
